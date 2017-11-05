d72a9c73bc014cbfbc24579434c22eff63f0a38b
maharshi <maharshijinandra@gmail.com>
2017-11-05 22:47:15 +0530

    [add] cartpole-v0 hill climbing algo

---
+++ cartpole_hill.py
@@ -0,0 +1,48 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+def run_episode(env, parameters):
+    observation = env.reset()
+    totalreward = 0
+    counter = 0
+    for _ in xrange(200):
+        # env.render()
+        action = 0 if np.matmul(parameters,observation) < 0 else 1
+        observation, reward, done, info = env.step(action)
+        totalreward += reward
+        counter += 1
+        if done:
+            break
+    return totalreward
+
+def train():
+    env = gym.make('CartPole-v0')
+
+    episodes_per_update = 5
+    noise_scaling = 0.1
+    parameters = np.random.rand(4) * 2 - 1
+    bestreward = 0
+    counter = 0
+
+    for _ in xrange(2000):
+        counter += 1
+        newparams = parameters + (np.random.rand(4) * 2 - 1)*noise_scaling
+        # print newparams
+        # reward = 0
+        # for _ in xrange(episodes_per_update):
+        #     run = run_episode(env,newparams)
+        #     reward += run
+        reward = run_episode(env,newparams)
+        # print "reward %d best %d" % (reward, bestreward)
+        if reward > bestreward:
+            # print "update"
+            bestreward = reward
+            parameters = newparams
+            if reward == 200:
+                break
+
+    return counter
+
+r = train()
+print r
